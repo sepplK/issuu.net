f2285e23a9f6603d638ccb76cc53d499b7ae5ed4
Sebastian König <s.koenig@prointernet.de>
2019-03-28 16:53:30 +0100

    Projektdateien hinzufügen.

---
+++ src/issuu.Client/IssuuClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace issuu_dotnet
+{
+    public class IssuuClient
+    {
+
+        public IssuuClient()
+        {
+
+        }
+
+        public IssuuClient(IssuuCredentials credentials)
+        {
+            Credentials = credentials;
+        }
+
+        public IssuuCredentials Credentials { get; set; }
+
+
+    }
+
+}
---
+++ src/issuu.Client/IssuuCredentials.cs
@@ -0,0 +1,9 @@
+﻿namespace issuu_dotnet
+{
+    public class IssuuCredentials
+    {
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+
+}
